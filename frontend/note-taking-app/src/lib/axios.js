167173a913114675fb1bee61c1c66af3bc0e95e6
Fatima86711 <fatimazafar411133@gmail.com>
2025-09-07 17:16:13 +0500

    updated

--- frontend/note-taking-app/src/lib/axios.js
+++ frontend/note-taking-app/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const api = axios.create({
     baseURL: 'http://localhost:3000/api'
 })
-
+// https://grocery-store-mu-virid.vercel.app/
 export default api;
 // process.env.NODE_ENV === 'production' 
 //     ? 'https://your-backend.vercel.app/api'
