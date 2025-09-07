1c7a84ddb7815dab5be22c64ce99c3d1fe9f5b48
Fatima86711 <fatimazafar411133@gmail.com>
2025-09-07 15:46:07 +0500

    updated

--- frontend/note-taking-app/src/lib/axios.js
+++ frontend/note-taking-app/src/lib/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: process.env.NODE_ENV === 'production' 
-    ? 'https://your-backend.vercel.app/api'
-    : 'http://localhost:3000/api',
+    baseURL: 'http://localhost:3000/api'
 })
 
 export default api;
+// process.env.NODE_ENV === 'production' 
+//     ? 'https://your-backend.vercel.app/api'
+//     : 'http://localhost:3000/api',
